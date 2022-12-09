93fa6ec36ed64b56141388ae0009d5a163d8ed25
siguretuki <siguretukikohane@gmail.com>
2022-12-09 16:07:14 +0900

    a

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,14 @@
+import Container from 'components/container'
 import Header from 'components/header'
 import Footer from 'components/footer'
+import { Component } from 'react'
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
 
-      <main>{children}</main>
+      <main><Container>{children}</Container></main>
 
       <Footer />
     </>
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,10 @@
 import Container from 'components/container'
 import Hero from "components/hero"
+import PostBody from 'components/post-body'
+import Contact from 'components/contact'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+import Image from 'next/image'
+import eyecatch from 'images/about.jpg'
 
 export default function About() {
   return (
@@ -8,7 +13,13 @@ export default function About() {
     title="About"
     subtitle="About development activities"
       />
+      <figure>
+        <Image src={eyecatch} alt='' layout='responsive' sizes='(min-width:1152px)1152px,100vw' priority placeholder='blur'/>
+      </figure>
 
+      <TwoColumn>
+        <TwoColumnMain>
+    <PostBody>
       <p>
         Cubeが得意とする分野はモノづくりです。3次元から2次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
       </p>
@@ -22,7 +33,14 @@ export default function About() {
       <h3>新しいことへのチャレンジ</h3>
       <p>
         今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
-      </p>
+        </p>
+      </PostBody>
+        </TwoColumnMain>
+        
+        <TwoColumnSidebar>
+        <Contact />
+        </TwoColumnSidebar>
+        </TwoColumn>
       </Container>
     )
 }
